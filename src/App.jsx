ffaadfb8b5455ffd8ecc6490265794a7616962ca
Anirudh A V <anirudh.av02@gmail.com>
2023-03-02 21:18:15 +0530

    feat: react route configurd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './Pages/Home'
+
 function App() {
 
   return (
-    <div></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
