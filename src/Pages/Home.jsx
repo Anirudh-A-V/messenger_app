e020ed203c9c3e9a459b227df6713f68e0c971d7
Anirudh A V <anirudh.av02@gmail.com>
2023-03-02 21:18:56 +0530

    Contact Bar UI first draft

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import ContactBar from "../Components/ContactBar"
+
+const Home = () => {
+
+    return (
+        <div className="flex flex-col min-h-[90vh] max-h-[95vh] bg-[#F0F2F5]">
+            <ContactBar />
+        </div>
+    )
+}
+
+export default Home
